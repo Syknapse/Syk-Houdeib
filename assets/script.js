2cc593adbee29640c9b3265a660263c75cb6973d
Syk Houdeib <syk.houdeib@gmail.com>
2017-09-17 14:19:20 +0200

    footer, contact style
    all style except projects completed.
    started a jQuery feature to clear header
    FIXED jQuery: nav-btn color not toggling after nav selection by removing class on nav click

--- assets/script.js
+++ assets/script.js
@@ -13,5 +13,14 @@ $(function(){
 	// refactor?? clicking anywhere on nav, and only on sections
     $('nav, section').on('click', function(){
         $('nav').removeClass('open');
+        $('#nav-btn').removeClass('open-btn');
     });
+});
+
+// add class to section when clicked from menu to create top padding
+$(function(){
+	$('nav a').on('click', function(){
+		$('section').removeClass('clear-header');
+		$($(this).attr('href')).addClass('clear-header');
+	});
 });
