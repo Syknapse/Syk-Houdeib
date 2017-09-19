f7883229e95fa79f68b95dbb91c0b3e74a251a63
Syk Houdeib <syk.houdeib@gmail.com>
2017-09-20 00:58:06 +0200

    project card PE logic and style completed
    Implemented logic for all buttons-description to work individually.
    implemented CSS to to create a swing-away effect of description on click

--- assets/script.js
+++ assets/script.js
@@ -27,41 +27,7 @@ $(function(){
 
 // project card clicking icon reveals highlights card
 
-// $(function(){
-// 	$('.plus').on('click', function(){
-// 		// $('section').removeClass('clear-header');
-// 		$('.project-card-description').toggleClass('swing-away');
-// 	});
-// });
-
-
 $('.plus').click(function(){
   var descriptionCard = '.description-' + $(this).data('project');
-  // $('.exp-info').not(infoPanel).hide();
   $(descriptionCard).toggleClass('swing-away');
 });
-
-// $('.exp-icons').click(function(){
-//   var infoPanel = '.exp-info-' + $(this).data('info');
-//   $('.exp-info').not(infoPanel).hide();
-//   $(infoPanel).toggle();
-
-
-// $(function(){
-// 	$('.plus').on('click', function(){
-// 		$('.project-card-description').removeClass('swing-away');
-//     	$(".project-card-description").eq($(this.element).index()).addClass('swing-away');
-// 	});
-// });
-
-
-	
-
-
-// var buttons = $('.plus');
-// for(var i = 0, i < buttons.length; i++){
-// 	buttons[i].on('click', function(){
-// 		// $('section').removeClass('clear-header');
-// 		$('.project-card-description').toggleClass('swing-away');
-// 	});
-// }
