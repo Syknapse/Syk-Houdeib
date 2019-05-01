a2333c044f45aa5fb5c028ba47fcadc39612343b
Syk Houdeib <syk.houdeib@gmail.com>
2019-05-01 19:33:59 +0200

    Create project card component and projects data module
    Make project cards modular creating a UI component and a data module

---
+++ src/data/projects.js
@@ -0,0 +1,18 @@
+const Projects = [
+  {
+    title: 'City/state search',
+    subtitle: 'Type-ahead search by US city or state name, see population',
+    technologies: ['Javascript', 'ES6'],
+    web_link: 'https://syknapse.github.io/Ajax-Type-Ahead',
+    github_link: 'https://github.com/Syknapse/Ajax-Type-Ahead'
+  },
+  {
+    title: 'ZZZZZeymourse',
+    subtitle: 'Type-ahead search by US city or state name, see population',
+    technologies: ['Javascript', 'ES6'],
+    web_link: 'https://syknapse.github.io/Ajax-Type-Ahead',
+    github_link: 'https://github.com/Syknapse/Ajax-Type-Ahead'
+  },
+]
+
+export default Projects
