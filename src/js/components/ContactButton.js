e2899a974c985d1cbd942a25adfac7cc93ebc50b
Syknapse <29199184+Syknapse@users.noreply.github.com>
2024-09-27 11:04:50 +0200

    Expand sections. Restyle Contact section

--- src/js/components/ContactButton.js
+++ src/js/components/ContactButton.js
@@ -2,6 +2,9 @@ export function ContactButton(contact) {
   let buttonTemplate = `<div class="${contact.style}">
     <a href="${contact.link}" target="_blank" title="${contact.title}">
         <i class="fa ${contact.icon} fa-fw" aria-hidden="true"></i>
+        <span class='contact-title' style="${contact.showTitle ? "display: inline" : "display: none"}">${
+    contact.title
+  }</span>
     </a>
 </div>`
 
